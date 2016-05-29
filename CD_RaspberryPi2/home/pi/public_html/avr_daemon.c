4161ebf59ecf742f7d63a45321ef86d9f9d28d86
janghe11 <jang0913@gmail.com>
2016-05-30 00:58:03 +0900

    Modified daemon process enabling / Check USB connect retry

--- CD_RaspberryPi2/home/pi/public_html/avr_daemon.c
+++ CD_RaspberryPi2/home/pi/public_html/avr_daemon.c
@@ -3,11 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 #include <sys/poll.h>
 #include <termios.h>                   
 #include <fcntl.h>
 #include <mysql.h>
 #include <time.h>
+#include <syslog.h>
 
 #define CHECKING_TIME 	1000							// Manipulate poll time out.
 
@@ -69,11 +72,14 @@ int avr_maria_set(int avr_get_count, char avr_read_content)
 	
 	mysql_close(&avr_maria);
 	
-	return 0;
+	return rpi_query_check;
 }
 
 int main(int argc, char* argv[])
 {        
+
+   pid_t pid, sid;								// Process id for avr_daemon.
+   
    int	tty_usb = -1; 					// Destination device(file) descriptor. Default : Not connected.
    int	read_check = -1;				// Check read() function read data (Default : -1, Data read : byte size, Not read : -1)
    unsigned char	buf[2] = {'\0'};	// Variable for receive data. (buf[0] : Real data, buf[1] : Noises after data)
@@ -81,12 +87,12 @@ int main(int argc, char* argv[])
    struct	pollfd	poll_events;        // Check polling event structure.
    int	poll_state = -1;				// Change poll_state when data is received. (Default : -1)
    int	avr_get_count = 0;				// Check count when data is sent from AVR (Set 0 when RPi reboot)
-
+   int maria_insert_check = -1;			// Check data inserted successfuly into mariaDB.
+	
    // Open ttyUSB0 (Read and Write, NO CTTY, NO BLOCK)
    tty_usb = open( "/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK );
    if (tty_usb < 0) {        
-      printf("Failed to connect ATmega8535. Please check RS232 cable connection again.\n");
-      return -1;
+      printf("Failed to connect ATmega8535. Please check RS232 cable connection. Try to reconnetct.\n");
    } else {
 	   printf("ATmega8535 is connected by ttyUSB0. Polling standby.\n");
    }
@@ -94,24 +100,60 @@ int main(int argc, char* argv[])
    // Serial port termios communication settings
    memset(&serial_settings, 0, sizeof(serial_settings));			// Initialize serial_settgins termios struct.
    serial_settings.c_cflag       = B9600 | CS8 | CLOCAL | CREAD;	// Control modes. (9600, 8bit data, No parity, 1 Stopbit, No Hardware and Software flow control)
-   serial_settings.c_oflag       = 0;	/* ? */						// Output modes.
-   serial_settings.c_lflag       = 0;	/* ? */						// Local modes.
+   serial_settings.c_oflag       = 0;								// Output modes.
+   serial_settings.c_lflag       = 0;								// Local modes.
    serial_settings.c_cc[VTIME]   = 0;	/* ? */						 
    serial_settings.c_cc[VMIN]    = 1;								// Minimum number of characters for non canonical read.
    
-   tcflush(tty_usb, TCIFLUSH ); /* ? */
-   tcsetattr(tty_usb, TCSANOW, &serial_settings );	/* ? */
-   fcntl(tty_usb, F_SETFL, FNDELAY); 	/* ? */
+   tcflush(tty_usb, TCIFLUSH ); 									// flushes data received but not read.
+   tcsetattr(tty_usb, TCSANOW, &serial_settings );					// Parameters associated with the terminal change occurs immediately.
+   fcntl(tty_usb, F_SETFL, FNDELAY); 								// Reset fd flag of ttyUSB0
 
    // Settings for polling
-   poll_events.fd        = tty_usb;					  // Get device(file) open location
-   poll_events.events    = POLLIN | POLLERR;          // Check poll in and error
-   poll_events.revents   = 0; 						  // Check returned event. Default : 0
+   poll_events.fd        = tty_usb;					  				// Get device(file) open location
+   poll_events.events    = POLLIN | POLLERR;          				// Check poll in and error
+   poll_events.revents   = 0; 						  				// Check returned event. Default : 0
 
+	// Make daemon process
+	pid = fork();						// Generate fork.
+	syslog(LOG_INFO, "Entering daemon");
+	printf("pid = [%d] \n", pid);
+	
+	if(pid < 0) {						// When fork error, make log and exit.
+		printf("fork function error : return value [%d]\n", pid);
+		exit(EXIT_FAILURE);
+	} else if(pid > 0) {				// Make parent process log and exit.
+		printf("Child process : [%d] - Parent process : [%d]\n", pid, getpid());
+		exit(EXIT_SUCCESS);
+	} else if(pid == 0) {				// When works normal, make log.
+		printf("Start process : [%d]\n", getpid());
+	}
+	
+	umask(0);							// Change file mask
+	sid = setsid();						// Give new seesion.
+	if(sid < 0) {
+		exit(EXIT_FAILURE);
+	}
+	
+	signal(SIGHUP, SIG_IGN);			// When terminal exit, process not affected by signal.
+	
+	if((chdir("/")) < 0) {
+		exit(EXIT_FAILURE);							// Change execution location to root.
+	}
+	
+	close(STDIN_FILENO);
+	//close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 
-   // Receive data from AVR and Save to avr_data.txt
+   // Receive data from AVR and insert data into table.
    while (1)
-   {
+   {   
+	   // When ttyUSB0 is not connected, try to reconnect.
+	   if(tty_usb < 0) {
+			tty_usb = open( "/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK );
+			printf("Failed to connect ATmega8535. Unplug RS-232C cable and reconnect. Try to reconnetct...\n");
+			sleep(1);
+	   }
 	  /*
 	   * Call poll() -> check event
        * poll(event registration var, Amount of checking pollfd (Only 1 in this file : tty_usb), set time out (1000 = 1sec))
@@ -125,8 +167,12 @@ int main(int argc, char* argv[])
             printf("buf[0] print : %c\n", buf[0]);
             
             avr_get_count++;
-			avr_maria_set(avr_get_count, (char)buf[0]);
-			printf("Data inserted to database. >> %d byte, %c\n", read_check, buf[0]);
+			maria_insert_check = avr_maria_set(avr_get_count, (char)buf[0]);
+			if(maria_insert_check == 0) {
+				printf("Data inserted to database. >> %d byte, %c\n", read_check, buf[0]);
+			} else {
+				printf("Data inserted failed. See details upper line. >> %d byte, %c\n", read_check, buf[0]);
+			}
 		 }
          
          if (poll_events.revents & POLLERR) {	//	Data corrupted
@@ -135,5 +181,6 @@ int main(int argc, char* argv[])
       }
    }
    close(tty_usb);
+   closelog();
    return 0;
 }
